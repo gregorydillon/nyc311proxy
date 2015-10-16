8f694a98d335eabdb7d9581b7730b8cf8af0f9d5
Chris Whong <chris@whong.org>
2015-10-15 23:40:40 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var moment = require('moment');
+var Mustache = require('mustache');
+var request = require('request');
+
+app.get('/', function (req, res) {
+
+  //get the date from 30 days ago
+  var thirtydaysago = moment().subtract(30, 'days').format('YYYY-MM-DD')
+
+    //build a SODA API call... eww
+    var apiCall = Mustache.render("https://data.cityofnewyork.us/resource/fhrw-4uyv.geojson?$where=created_date>='{{date}}'&$limit=500000",{date:thirtydaysago});
+
+    //GET the API call and pipe it to the response
+    request.get(apiCall).pipe(res);
+});
+
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+
+  console.log('Example app listening on port  ',port);
+});
