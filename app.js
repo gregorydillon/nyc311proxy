b9743acc374862466dc1c51e0a9b9f4587d00118
Chris Whong <chris@whong.org>
2016-03-12 00:54:57 -0500

    fixed url template

--- app.js
+++ app.js
@@ -4,17 +4,22 @@ var moment = require('moment');
 var Mustache = require('mustache');
 var request = require('request');
 
-app.get('/', function (req, res) {
+app.get('/:anything', function (req, res) {
 
   var sourceLimit = 1000000;
   //get the date from 90 days ago
   var ninety_days_ago = moment().subtract(90, 'days').format('YYYY-MM-DD')
 
-  var sourceTemplate = 'https://data.cityofnewyork.us/resource/fhrw-4uyv.csv?$LIMIT={{sourceLimit}}&$ORDER=created_date%20DESC&$WHERE=created_date>=%272015-12-12%27';
+  var sourceTemplate = 'https://data.cityofnewyork.us/resource/fhrw-4uyv.csv?$LIMIT={{sourceLimit}}&$ORDER=created_date%20DESC&$WHERE=created_date>=%27{{ninety_days_ago}}%27';
 
 
     //build a SODA API call... eww
-    var sourceURL = Mustache.render( sourceTemplate, { sourceLimit: sourceLimit });
+    var sourceURL = Mustache.render( sourceTemplate, { 
+      sourceLimit: sourceLimit,
+      ninety_days_ago: ninety_days_ago 
+    });
+
+    console.log(sourceURL);
 
     //GET the API call and pipe it to the response
     request.get(sourceURL).pipe(res);
